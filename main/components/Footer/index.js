3ecdc214e03c7c1dd070ef3ab327614e52970a29
Pavel <serpork@ya.ru>
2020-05-21 21:09:59 +0700

    fix styles

--- main/components/Footer/index.js
+++ main/components/Footer/index.js
@@ -20,19 +20,22 @@ export default observer(function Footer ({ icons }) {
         View.row
           View.label
             Text(white bold) Address
-          Text(white) 1234 Somewhere Road • Nashville, TN 00000 • USA
+          View.value
+            Text(white) 1234 Somewhere Road • Nashville, TN 00000 • USA
         View.row
           View.label
             Text(white bold) Phone
-          Text(white) (000) 000-0000 x 0000
+          View.value
+            Text(white) (000) 000-0000 x 0000
         View.row
           View.label
             Text(white bold) Email
-          Text(white) information@untitled.tld
+          View.value
+            Text(white) information@untitled.tld
       View.icons
         each icon in icons
           View.iconWrapper(key=icon)
-            Image.icon(source={uri: BASE_URL + 'social' + icon})
+            Image.icon(source={uri: BASE_URL + '/social' + icon})
       View.credits
         Text(white) © Untitled. Design: HTML5 UP. Demo Images: Unsplash.
   `
